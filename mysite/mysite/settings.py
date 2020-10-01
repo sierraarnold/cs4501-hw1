538651b3b0d94e7b6dc14ecbbd2269e1a4d5bb56
Sierra Arnold <sla7zh@virginia.edu>
2020-09-30 20:52:19 -0400

    more fix heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'c(b+!xp0v@usw7rjj@thzq&ull88#d%yk@zx-f88#dv*0on33='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost','127.0.0.1']
 
 
 # Application definition
